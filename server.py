63d760b727a99a1558b94cfd883805a428b4c4a5
Pellpoy61 <152411607+Pellpoy61@users.noreply.github.com>
2024-02-08 00:27:56 +0200

    server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/receive_duckyscript', methods=['POST'])
 def receive_duckyscript():
@@ -9,7 +11,8 @@ def receive_duckyscript():
     with open('/home/elliot/received_duckyscript.txt', 'w') as file:
         file.write(duckyscript)
         # Execute Duckyscript or perform desired actions
+
     return 'Duckyscript received successfully!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='192.168.10.111', port=8080, ssl_context=('cert.pem', 'key.pem'))
