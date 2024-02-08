0f8357803fb994f0a8b3639bbc3b2200980904a4
Pellpoy61 <152411607+Pellpoy61@users.noreply.github.com>
2024-02-08 02:04:08 +0200

    server.py

--- server.py
+++ server.py
@@ -5,15 +5,19 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/receive_duckyscript', methods=['POST'])
-@cross_origin()
 def receive_duckyscript():
-    duckyscript = request.form.get('duckyscript')
-    # Process the received Duckyscript (e.g., save to a file and execute)
-    with open('/home/elliot/received_duckyscript.txt', 'w') as file:
-        file.write(duckyscript)
-        # Execute Duckyscript or perform desired actions
+    try:
+        duckyscript = request.form.get('duckyscript')
+        
+        # Process the received Duckyscript (e.g., save to a file and execute)
+        with open('/home/elliot/received_duckyscript.txt', 'w') as file:
+            file.write(duckyscript)
+            # Execute Duckyscript or perform desired actions
 
-    return 'Duckyscript received successfully!'
+        return 'Duckyscript received successfully!'
+    except Exception as e:
+        print(f'Error processing Duckyscript: {e}')
+        return 'Error processing Duckyscript', 500  # Return a 500 Internal Server Error response
 
 if __name__ == '__main__':
     app.run(host='192.168.10.111', port=8080, ssl_context=('cert.pem', 'key.pem'))
